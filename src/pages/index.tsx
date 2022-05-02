3c38a4b80c561c2adfaad45f7691bb740b6e774c
ryansldev <ryanlima1309@gmail.com>
2022-05-02 15:23:44 -0300

    :tada: Starting project

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>NLW Return</title>
+        <meta name="description" content="NLW Return" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main>NLW Return</main>
+    </div>
+  );
+};
+
+export default Home;
