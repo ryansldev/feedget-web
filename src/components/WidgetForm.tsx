80f44469f7a882442721076ea86d672ed68ac43d
ryansldev <ryanlima1309@gmail.com>
2022-05-03 16:58:35 -0300

    feat: add CloseButton component to WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 
+import { CloseButton } from "@components/CloseButton";
 import Link from "next/link";
 
 export const WidgetForm: FC = () => {
@@ -7,6 +8,8 @@ export const WidgetForm: FC = () => {
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
       </header>
 
       <p>Hello world</p>
