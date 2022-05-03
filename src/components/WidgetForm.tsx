c4fcd094e2b41b74ed6e03876eed2c61c0f46325
ryansldev <ryanlima1309@gmail.com>
2022-05-03 10:17:36 -0300

    feat: add header and footer to WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,24 @@
-export function WidgetForm() {
+import type { FC } from "react";
+
+import Link from "next/link";
+
+export const WidgetForm: FC = () => {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+      </header>
+
+      <p>Hello world</p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{" "}
+        <Link href="https://rocketseat.com.br" passHref>
+          <a className="underline underline-offset-1" target="_blank">
+            Rocketseat
+          </a>
+        </Link>
+      </footer>
     </div>
   );
-}
+};
