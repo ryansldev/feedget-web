2a9c8121295e93cbd2813ee77d06c5031a209400
ryansldev <ryanlima1309@gmail.com>
2022-05-03 22:20:13 -0300

    feat: add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import { CircleNotch } from "phosphor-react";
+
+export const Loading: FC = () => {
+  return (
+    <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
+      <CircleNotch weight="bold" className="w-4 h-4 animate-spin" />
+    </div>
+  );
+};
